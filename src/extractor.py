77453e3710f23305016d5cecfc64603bf591eb90
Xin.Yu <sinceyx@outlook.com>
2022-02-11 02:10:21 +0800

    create requirements.txt to manage dependency

---
+++ src/extractor.py
@@ -0,0 +1,11 @@
+import jq
+import json
+
+
+def extract(dic: dict, jq_path: str):
+    """
+        Return the value given the dictionary and jq_path
+        If the path does not exist, return NONE
+    """
+    return json.loads(jq.compile(jq_path).input(dic).text())
+
