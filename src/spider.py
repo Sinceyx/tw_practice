8be0d3dd17467dc405ebed8af2e9c9c6520ce289
Xin.Yu <sinceyx@outlook.com>
2022-02-14 03:21:07 +0800

    refactor spider.py and rewrite unittest of spider.py

--- src/spider.py
+++ src/spider.py
@@ -3,56 +3,53 @@
 """
 import re
 import json
+
 import requests
 from extractor import extract
 
 
-class AtrealtyWebSpider(object):
-    __url = 'https://www.atrealty.com.au/wp-json/hi-api/v1/properties?page=1&per_page=12&listing=residential' \
-            '&price_type=sale&tags_field=&search-id=983&auction=&inspection=&location=&sub_type=any&min_price' \
-            '=&max_price=&min_bedrooms=&max_bedrooms=&min_bathrooms=&max_bathrooms=&parking=&surrounding_suburbs=1' \
-            '&suburb=&state=&latlng= '
-    page_size = 5
-    data_array = {}
-
-    def __init__(self, p):
-        self.page_size = p
-        self.data_array = self.__capture_product_infos_from_atrealty(p)
-
-    def __capture_product_infos_from_atrealty(self, page_size: int):
-        result = json.loads(requests.get(self.__url.replace('per_page=12', 'per_page='+str(page_size))).text)
-        return extract(result, '.data')
+def crawl():
+    website_data_json = json.loads(__fetch_website_data())
+    full_address = __extract_full_house_address(website_data_json)
+    return __format_house_address(full_address)
 
 
-def extract_address(arealty_web_spider: AtrealtyWebSpider):
-    address_array = []
-    for item in arealty_web_spider.data_array:
-        address_array.append(extract(item, '.name'))
-    return address_array
+def __fetch_website_data():
+    url = 'https://www.atrealty.com.au/wp-json/hi-api/v1/properties?page=1&per_page=5&listing=residential' \
+          '&price_type=sale&tags_field=&search-id=983&auction=&inspection=&location=&sub_type=any&min_price' \
+          '=&max_price=&min_bedrooms=&max_bedrooms=&min_bathrooms=&max_bathrooms=&parking=&surrounding_suburbs=1' \
+          '&suburb=&state=&latlng='
+    return requests.get(url).text
 
 
-def extract_house_address_infos(arealty_web_spider: AtrealtyWebSpider):
-    state_name_array = []
-    postal_codes_array = []
-    permalink_array = []
-    for item in arealty_web_spider.data_array:
-        permalink = extract(item, '.permalink')
-        permalink_array.append(permalink)
-        permalink_pattern = re.compile('/?[^./][0-9a-z-]*/?$')
-        permalink_regex_search_result = permalink_pattern.search(permalink).group().replace('/', '')
-        state_name_array.append(__extract_state_name_from_permalink(permalink_regex_search_result))
-        postal_codes_array.append(__extract_postal_code_from_permalink(permalink_regex_search_result))
-    house_info_array = {'state_names': state_name_array, 'postal_codes': postal_codes_array, 'permalinks': permalink_array }
-    return house_info_array
+def __extract_full_house_address(data: json):
+    full_address_array = []
+    permalink_pattern = re.compile('/[a-z-0-9-]*[0-9]{4}-[0-9]{5}/$')
+    for item in extract(data, '.data'):
+        full_address_array.append(permalink_pattern.search(extract(item, '.permalink')).group().replace('/', ''))
+    return full_address_array
 
 
-def __extract_state_name_from_permalink(permalink: str):
-    state_name_pattern = re.compile('^[1-9]?[0-9-]*[a-z-0-9]*[a-z]')
-    search_result = state_name_pattern.search(permalink).group()
-    return search_result
+def __format_house_address(address_array: list):
+    result_address_array = []
+    for ele in address_array:
+        result_address = ''
+        ele = ele.upper()
+        result_address += __parse_house_number(ele) + ' '
+        result_address += __parse_other_part(ele)
+        result_address_array.append(result_address)
+    return result_address_array
 
 
-def __extract_postal_code_from_permalink(permalink: str):
-    postal_code_pattern = re.compile('[0-9]{4}-[0-9]{5}')
-    search_result = postal_code_pattern.search(permalink).group()
-    return search_result
+def __parse_house_number(s: str):
+    house_number = re.compile('^[0-9]?-?[0-9]+').search(s).group()
+    if '-' in house_number:
+        return 'UNIT ' + house_number.replace('-', ' ')
+    return house_number
+
+
+def __parse_other_part(s: str):
+    return re.compile('[A-Z][A-Z-]*-[0-9]{4}').search(s).group().replace('-', ' ')
+
+
+
--- tests/test_spider.py
+++ tests/test_spider.py
@@ -3,34 +3,22 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import unittest
-from unittest import mock
-from read_json_util import read_json_file
-from spider import extract_house_address_infos, extract_address
+from spider import crawl
 
 
 class TestSpider(unittest.TestCase):
 
-	@mock.patch("spider.AtrealtyWebSpider")
-	def test_extract_house_address_infos(self, mock_atrealty_spider):
-		fake_spider = mock_atrealty_spider.return_value
-		fake_spider.data_array = read_json_file('../resource/test_spider.json')
-		expected_state_names = ['hay-nsw', '15-kingfisher-lane-congarinni-north-nsw', '30-iluka-avenue-san-remo-nsw', '3-9-ridge-street-nambucca-heads-nsw', '71-172-parramatta-road-homebush-nsw']
-		expected_postal_codes = ['2711-40013', '2447-40083', '2262-40076', '2448-40087', '2140-40015']
-		expected_permalinks = ['https://www.atrealty.com.au/hay-nsw-2711-40013/', 'https://www.atrealty.com.au/15-kingfisher-lane-congarinni-north-nsw-2447-40083/', 'https://www.atrealty.com.au/30-iluka-avenue-san-remo-nsw-2262-40076/', 'https://www.atrealty.com.au/3-9-ridge-street-nambucca-heads-nsw-2448-40087/', 'https://www.atrealty.com.au/71-172-parramatta-road-homebush-nsw-2140-40015/']
+	def test_crawl(self):
+		call_return = crawl()
 
-		actual_result = extract_house_address_infos(fake_spider)
+		expected_type = list
+		actual_return_type = type(call_return)
+		self.assertTrue(expected_type, actual_return_type)
 
-		actual_state_names = actual_result.get('state_names')
-		actual_postal_codes = actual_result.get('postal_codes')
-		actual_permalinks = actual_result.get('permalinks')
+		expected_size = 5
+		actual_size = len(call_return)
+		self.assertEqual(expected_size, actual_size)
 
-		self.assertTrue(str(expected_state_names)==str(actual_state_names) and str(expected_postal_codes)==str(actual_postal_codes) and str(expected_permalinks)==str(actual_permalinks))
 
-	@mock.patch("spider.AtrealtyWebSpider")
-	def test_extract_address(self, mock_atrealty_spider):
-		fake_spider = mock_atrealty_spider.return_value
-		fake_spider.data_array = read_json_file('../resource/test_spider.json')
-		actual_result = extract_address(fake_spider)
-		expected_result =['Hay, NSW, 2711', '15 Kingfisher Lane, Congarinni North, NSW, 2447', '30 Iluka Avenue, San Remo, NSW, 2262', '3/9 Ridge Street, Nambucca Heads, NSW, 2448', '71/172 Parramatta Road, Homebush, NSW, 2140']
-		self.assertEqual(expected_result, actual_result)
+
 
